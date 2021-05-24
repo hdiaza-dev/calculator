5a11c792bd6b977440aa72cf0239d14a3c86abf1
Héctor Díaz Alonso <h.diaz.alonso@gmail.com>
2021-05-24 09:29:49 +0200

    Agrego traceo de resultados.

--- src/main/java/com/hdiaza/calculator/web/CalculatorController.java
+++ src/main/java/com/hdiaza/calculator/web/CalculatorController.java
@@ -12,24 +12,42 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.hdiaza.calculator.domain.Operator;
 import com.hdiaza.calculator.services.CalculatorService;
 
+import io.corp.calculator.TracerImpl;
+
 /**
- * @author hdiaz
+ * The Class CalculatorController.
  *
+ * @author hdiaz
  */
 @Controller
 public class CalculatorController {
 
+	/** The Constant CALCULATOR_ADD. */
 	public static final String CALCULATOR_ADD = "/operate";
 
+	/** The Constant impl. */
+	private static final TracerImpl impl = new TracerImpl();
+
+	/** The calculator service. */
 	@Autowired
 	private CalculatorService calculatorService;
 
+	/**
+	 * Operate.
+	 *
+	 * @param operator the operator
+	 * @return the response entity
+	 */
 	@RequestMapping(value = CALCULATOR_ADD, method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Double> operate(@RequestBody Operator operator) {
 		if (!calculatorService.isValidOperator(operator)) {
+			impl.trace("BadRequest de operator ".concat(operator.toString()));
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		}
-		return new ResponseEntity<>(calculatorService.getOperationValue(operator), HttpStatus.OK);
+		Double operationValue = calculatorService.getOperationValue(operator);
+		impl.trace("Resultado de la operación ".concat(operator.toString()).concat(" es ")
+				.concat(String.valueOf(operationValue)));
+		return new ResponseEntity<>(operationValue, HttpStatus.OK);
 	}
 
 }
